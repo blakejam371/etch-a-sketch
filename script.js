44c1ca6708c330a91df595de9dd3a642fcf9e3a4
James Blake <142352216+blakejam371@users.noreply.github.com>
2024-03-16 17:58:05 +0000

    Add 'Reset Game' function
    - Add button to top of page with 5vh
    - Add click event listener to run resetGame funct
    - Change container div to 95vh
    - Create resetGame function which:
      - Deletes existing grid
      - Prompts for new grid size
      - Creates new grid
      - Adjusts box sizes to accomodate new grid size

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const rows = 16; 
-const columns = 16;
-
+let rows = 16; 
+let columns = 16;
 const gridContainer = document.querySelector('#container');
+const resetButton = document.querySelector('#reset-button');
+createGrid();
 
-for (let i = 0; i < rows; i++) {
-  for (let j = 0; j < columns; j++) {
-    const gridItem = document.createElement('div');
-    gridItem.className = 'grid-item';
-    gridItem.textContent = `${i + 1}-${j + 1}`;
-    gridContainer.appendChild(gridItem);
-    gridItem.addEventListener('mouseover', () => {
-      gridItem.style.backgroundColor = 'orchid';
-      gridItem.style.opacity = '0.6';
-    });
+function createGrid() {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      resetButton.addEventListener('click', resetGame);
+      const gridItem = document.createElement('div');
+      gridItem.className = 'grid-item';
+      gridItem.style.flex = `${100 / columns}%`;
+      gridContainer.appendChild(gridItem);
+      gridItem.addEventListener('mouseover', () => {
+        gridItem.style.backgroundColor = 'palegreen';
+        gridItem.style.opacity = '0.6';
+      });
+    }
   }
-}
+};
+
+// function to reset game with new grid size
+function resetGame() {
+  while (gridContainer.hasChildNodes()) {
+    gridContainer.removeChild(gridContainer.children[0]);
+  }
+  let newSize = prompt('What size grid do you want next?', '');
+  rows = newSize;
+  columns = newSize;
+  createGrid();
+};
+
